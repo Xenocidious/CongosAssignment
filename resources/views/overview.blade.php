8c851614529ffaf16c2b800851edcaf23c8698a5
default <Gismogy@virtuostudios.com>
2024-09-05 22:45:32 +0000

    Wrapped the overview in a div

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -6,26 +6,27 @@
 
         <title>Congos Person Manager</title>
 
-        <link rel="stylesheet" href="app.css">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
-    <body>
-        @foreach($persons as $person)
-            <p>{{ $person->name }} {{ $person->surname }}</p>
-            <ul>
-                @foreach($person->addresses as $address)
-                    <li>Address: {{ $address->street_name }} {{ $address->street_number }}, {{ $address->city }}</li>
-                    <ul>
-                        @foreach($address->notes as $note)
-                            <li>Note: {{ $note->note }}</li>
-                        @endforeach
-                    </ul>
-                @endforeach
-            </ul>
-            <ul>
-                @foreach($person->notes as $note)
-                    <li>Note: {{ $note->note }}</li>
-                @endforeach
-            </ul>
-        @endforeach
+        <div class="personContainer">
+            @foreach($persons as $person)
+                <p>{{ $person->name }} {{ $person->surname }}</p>
+                <ul>
+                    @foreach($person->addresses as $address)
+                        <li>Address: {{ $address->street_name }} {{ $address->street_number }}, {{ $address->city }}</li>
+                        <ul>
+                            @foreach($address->notes as $note)
+                                <li>Note: {{ $note->note }}</li>
+                            @endforeach
+                        </ul>
+                    @endforeach
+                </ul>
+                <ul>
+                    @foreach($person->notes as $note)
+                        <li>Note: {{ $note->note }}</li>
+                    @endforeach
+                </ul>
+            @endforeach
+        </div>
     </body>
 </html>
